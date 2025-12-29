5198474263e9a0479ff29b00d7c5b557c440e2cb
ceze88 <dev.ceze@gmail.com>
2025-12-29 15:32:54 +0100

    Update NMS for 1.21.11 - Paper removed recentTps field

--- NMS/NMS-v1_21_R7/src/main/java/com/songoda/core/nms/v1_21_R7/server/NmsServerImpl.java
+++ NMS/NMS-v1_21_R7/src/main/java/com/songoda/core/nms/v1_21_R7/server/NmsServerImpl.java
@@ -4,9 +4,22 @@ import com.songoda.core.nms.server.NmsServer;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_21_R7.CraftServer;
 
+import java.lang.reflect.Method;
+
 public class NmsServerImpl implements NmsServer {
     @Override
     public double[] getRecentTps() {
-        return ((CraftServer) Bukkit.getServer()).getServer().recentTps;
+        try {
+            return ((CraftServer) Bukkit.getServer()).getServer().recentTps;
+        } catch (Exception e) {
+            // Paper removed recentTps field, try Bukkit.getServer().getTPS()
+            try {
+                Object server = Bukkit.getServer();
+                Method getTpsMethod = server.getClass().getMethod("getTPS");
+                return (double[]) getTpsMethod.invoke(server);
+            } catch (Exception ex) {
+                throw new RuntimeException("Unable to get recent TPS", ex);
+            }
+        }
     }
 }
