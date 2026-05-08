c63d882d3e1ab7c3081653254b012b598c33c519
TeamHRLive <hsingh20sep@gmail.com>
2026-05-08 11:18:20 +0530

    [Added] Missing Major Version.

--- Compatibility/src/main/java/com/songoda/core/compatibility/MajorServerVersion.java
+++ Compatibility/src/main/java/com/songoda/core/compatibility/MajorServerVersion.java
@@ -3,7 +3,8 @@ package com.songoda.core.compatibility;
 import org.apache.commons.lang3.ArrayUtils;
 
 public enum MajorServerVersion {
-    UNKNOWN, V1_7, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17, V1_18, V1_19, V1_20, V1_21, V1_22, V1_23;
+    UNKNOWN, V1_7, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17, V1_18, V1_19, V1_20, V1_21, V1_22,
+    V26_1;
 
     private static final MajorServerVersion SERVER_VERSION = getVersion();
 
